f5706b124475fd64d754f83786a85844b7d08a26
wallst-code <senit@senitlutgen.com>
2021-12-20 19:25:26 -0800

    Clock app doesn't work yet

--- clock_app/app.js
+++ clock_app/app.js
@@ -1,14 +1,40 @@
-(function(app) {
-        'use strict';
-        const pageItems = {};
-    //    console.log('hello clock');
+// (function (app) {
+//   "use strict";
+//   const pageItems = {};
 
-       function helloTime(){
-           console.log('hello time');
-       }
+// })((window.app = window.app || {}));
+'use strict';
+const pageItems = {};
+const myTime = new Date();
+
+function clockStartup(){
     
-    })(window.app = window.app || {});
+    console.log(myTime.getTime());
+    console.log(myTime.getHours());
+    console.log(myTime.getMinutes());
+    console.log(myTime.getSeconds());
+    console.log(myTime.getDay());
+    console.log(myTime.getMonth());
+    console.log(myTime.getDate());
+    console.log(myTime.getFullYear());
+    console.log(myTime.getTimezoneOffset());
+    console.log(myTime.getUTCHours());
+    createClock();
+  }
 
-    function helloTime(){
-        console.log('hello time');
-    }
+function createClock(){
+    // pageItems.clock = document.getElementById('clock');
+    pageItems.timeBlock = document.getElementById('timeBlock');
+
+    pageItems.timeBlock.addEventListener('onload', grabTime);
+    
+}
+
+function grabTime(){
+    const p = document.createElement('p');
+    p.innerText = pageItems.myTime.getTime();
+    pageItems.timeBlock.appendChild(p);
+
+}
+//Does not work
+//TODO 
