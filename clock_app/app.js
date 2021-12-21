737658bb03415e99f1260ce873d6ee92fd4ca1db
wallst-code <senit@senitlutgen.com>
2021-12-20 18:48:08 -0800

    my clock disaster

---
+++ clock_app/app.js
@@ -0,0 +1,14 @@
+(function(app) {
+        'use strict';
+        const pageItems = {};
+    //    console.log('hello clock');
+
+       function helloTime(){
+           console.log('hello time');
+       }
+    
+    })(window.app = window.app || {});
+
+    function helloTime(){
+        console.log('hello time');
+    }
