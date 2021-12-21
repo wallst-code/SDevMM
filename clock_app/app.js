861991e71174abb88c23c056c879dddd5202f3fb
wallst-code <senit@senitlutgen.com>
2021-12-21 12:57:42 -0800

    Clock App

--- clock_app/app.js
+++ clock_app/app.js
@@ -3,38 +3,40 @@
 //   const pageItems = {};
 
 // })((window.app = window.app || {}));
-'use strict';
-const pageItems = {};
-const myTime = new Date();
 
-function clockStartup(){
+
+
+// function clockStartup(){
     
-    console.log(myTime.getTime());
-    console.log(myTime.getHours());
-    console.log(myTime.getMinutes());
-    console.log(myTime.getSeconds());
-    console.log(myTime.getDay());
-    console.log(myTime.getMonth());
-    console.log(myTime.getDate());
-    console.log(myTime.getFullYear());
-    console.log(myTime.getTimezoneOffset());
-    console.log(myTime.getUTCHours());
-    createClock();
-  }
+//     console.log(myTime.getTime());
+//     console.log(myTime.getHours());
+//     console.log(myTime.getMinutes());
+//     console.log(myTime.getSeconds());
+//     console.log(myTime.getDay());
+//     console.log(myTime.getMonth());
+//     console.log(myTime.getDate());
+//     console.log(myTime.getFullYear());
+//     console.log(myTime.getTimezoneOffset());
+//     console.log(myTime.getUTCHours());
+//     createClock();
+//   }
 
-function createClock(){
-    // pageItems.clock = document.getElementById('clock');
-    pageItems.timeBlock = document.getElementById('timeBlock');
+function showTime(){
+    'use strict';
+    const date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    let dayOrNight = 'AM';
+    console.log('hello');
 
-    pageItems.timeBlock.addEventListener('onload', grabTime);
-    
+    //if statements controlling adding 0 in front, and AM or PM
+
+    let myTime = `${hour}:${minute}:${second} ${dayOrNight}`
+
+    document.getElementById('clock').innerText = myTime;
+
+    //make the seconds tick...
 }
 
-function grabTime(){
-    const p = document.createElement('p');
-    p.innerText = pageItems.myTime.getTime();
-    pageItems.timeBlock.appendChild(p);
-
-}
-//Does not work
-//TODO 
+// Grab an element, create an element, populate it with info, build logic behind, wire it up. 
